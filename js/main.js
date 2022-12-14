8147783d123c7f67eb6088d550ecb8d3e3c67bb6
Victor <manuelher1619@gmail.com>
2022-12-14 13:09:39 -0600

    Se agregaron etiquetas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// aquí va el código
+// Campo producto - Name
+// Campo cantidad -Number
+// Boton agregar btnAgregar
+//alertaValidaciones
+// ContadorProductos
+//productosTotal
+//precioTotal
+// tabla tablaListaCompras
+
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+let total = document.getElementById("precioTotal");
+let tabla = document.getElementById("tablaListaCompras");
+let cuerpoTabla = tabla.getElementsByTagName("tbody");
+let btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener ("click", function(event) {
+    event.preventDefault();
+});
